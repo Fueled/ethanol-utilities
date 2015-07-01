59dbde9738405f1937487e9d637a312ef2eaab0a
stephane-fueled <stephane@fueled.com>
2015-07-01 11:53:26 -0400

    fix(umbrellaHeader): fix umbrella header #import style

--- EthanolUtilities/EthanolUtilities.h
+++ EthanolUtilities/EthanolUtilities.h
@@ -16,13 +16,13 @@ FOUNDATION_EXPORT const unsigned char EthanolUtilitiesVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <EthanolUtilities/PublicHeader.h>
 
-#import <CADisplayLink+EthanolBlocks.h>
-#import <NSTimer+EthanolBlocks.h>
-#import <ETHPreprocessorUtils.h>
-#import <NSArray+Ethanol.h>
-#import <NSDictionary+Ethanol.h>
-#import <NSObject+EthanolExecuteOnDealloc.h>
-#import <NSObject+EthanolSwizzling.h>
-#import <NSSet+Ethanol.h>
-#import <NSString+EthanolUtils.h>
-#import <NSTimer+EthanolBlocks.h>
+#import <EthanolUtilities/CADisplayLink+EthanolBlocks.h>
+#import <EthanolUtilities/NSTimer+EthanolBlocks.h>
+#import <EthanolUtilities/ETHPreprocessorUtils.h>
+#import <EthanolUtilities/NSArray+Ethanol.h>
+#import <EthanolUtilities/NSDictionary+Ethanol.h>
+#import <EthanolUtilities/NSObject+EthanolExecuteOnDealloc.h>
+#import <EthanolUtilities/NSObject+EthanolSwizzling.h>
+#import <EthanolUtilities/NSSet+Ethanol.h>
+#import <EthanolUtilities/NSString+EthanolUtils.h>
+#import <EthanolUtilities/NSTimer+EthanolBlocks.h>
