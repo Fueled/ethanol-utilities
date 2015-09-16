254bdbb7cae001a8f41a9cc3e9e886f59178f0f9
harish-fueled <harish@fueled.com>
2015-09-16 18:32:05 +0530

    feat(resourceFetcher) : Test Cursor based Pagination

--- EthanolUtilitiesTests/TestResourceFetcher.swift
+++ EthanolUtilitiesTests/TestResourceFetcher.swift
@@ -42,3 +42,54 @@ class TestResourceFetcher: ResourceFetcher {
 		}
 	}
 }
+
+class TestCursorResourceFetcher: CurserBasedResourceFetcher {
+
+	var delayTime = 2.0
+	var lastPageURL = 5.stringValue
+	var successChance = 90 // percent success chance
+
+	override func fetchPage(url: String?, pageLimit: Int, completion: CursorBasedAPICompletionHandler?){
+
+
+		let delay = dispatch_time(DISPATCH_TIME_NOW, Int64(delayTime * Double(NSEC_PER_SEC)))
+		dispatch_after(delay, dispatch_get_main_queue()) {
+
+			var randomArray: [String] = []
+
+			let pageNumber = url?.intValue ?? 0
+
+			if (pageNumber == self.lastPageURL.intValue) { // Last Page
+				for index in 0..<pageLimit-1 {
+					randomArray.append("Page - \(pageNumber) - \(index)")
+				}
+			} else {
+				for index in 0..<pageLimit {
+					randomArray.append("Page - \(pageNumber) - \(index)")
+				}
+			}
+
+			let error = NSError(domain: "APIERROR", code: 400, userInfo: [NSLocalizedDescriptionKey : "NOTHING ERROR"])
+
+			let success = (random() % 100 <= self.successChance)
+			if success {
+				completion?(){ return ( "1" , nil, randomArray) }
+			} else {
+				completion?(){ throw error }
+			}
+		}
+
+	}
+}
+
+extension String {
+	var intValue: Int {
+		return Int(self) ?? 0
+	}
+}
+
+extension Int {
+	var stringValue: String {
+		return String(self)
+	}
+}
